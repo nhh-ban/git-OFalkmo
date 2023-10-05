1ee1a21b4d3276b841096ad8243c36c185123d52
Ole Kristian Falkmo <olekristianfa@hotmail.com>
2023-10-05 11:53:49 +0200

    added comments, and completed task 3

--- Script/Assigment5.R
+++ Script/Assigment5.R
@@ -32,14 +32,25 @@ find_header_lines <- function(file_path, header_pattern = "--") {
   return(line_count-2)
 }
 
+#Read the datafile and skip lines determinded by the function above
 data <- read.csv("../Data/suites_dw_Table1.txt", sep ="|", 
                  skip = find_header_lines("../Data/suites_dw_Table1.txt")) %>% 
   .[-1,]
 
+#Make a plot of galaxy size in regards to distance from Earth. 
 data %>% 
   ggplot(aes(x = D, y = log_lk)) + 
   geom_point()+ 
   geom_smooth() +
   xlab("Distance to galaxy in Mpc (D)") + 
-  ylab("logarithm of the stellar mass (log_lk)")
-
+  ylab("logarithm of the stellar mass (log_lk)") + 
+  theme_classic()
+ 
+#From the plot we observe that the observation of small galaxies 
+#diminishes as the distance become grater.
+#From distance of close to 0 we have several observations below 5.0, but
+#these completely disperse when the distance become greater. Under the assumption
+#that there would be a equal distribution of galaxy of all sizes throughout the
+#universe we would belive there to be small galaxies further away from earth, 
+#not only close to Earth. These observation could be caused by our 
+#technology not being sufficient to see smaller galaxies from a greater distance. 
